5e7394738536238817b076f9dcd025fe448ea735
leesj000603 <leesj000603@daum.net>
2024-06-25 17:53:28 +0900

    알파벳 대소문자 변환문제

---
+++ src/backjoon/Java/bronze/solutions/alphabetconvert/Main.java
@@ -0,0 +1,34 @@
+package backjoon.Java.bronze.solutions.alphabetconvert;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Solution {
+  public void solution() throws IOException {
+    System.setIn(new FileInputStream("C:\\Users\\leeseungjune\\IdeaProjects\\coding-test\\src\\backjoon\\Java\\bronze\\solutions\\alphabetconvert\\input.txt"));
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String[] s = bf.readLine().split("");
+    for (int i=0; i<s.length;i++){
+      int ascii = (byte)s[i].charAt(0);
+      if(ascii<97){
+        bw.write(s[i].toLowerCase());
+      } else {
+        bw.write(s[i].toUpperCase());
+      }
+    }
+    bw.flush();
+  }
+}
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Solution s = new Solution();
+    s.solution();
+  }
+
+}
