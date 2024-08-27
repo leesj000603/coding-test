7485f7bb24fc5b7999f7e0793271a4bc80ba51a7
leesj000603 <leesj000603@daum.net>
2024-08-27 17:53:07 +0900

    문제 풀이 진행사항 저장

--- src/backjoon/Java/silver/solutions/absolutevalueheap/Main.java
+++ src/backjoon/Java/silver/solutions/absolutevalueheap/Main.java
@@ -1,17 +1,101 @@
 package backjoon.Java.silver.solutions.absolutevalueheap;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.PriorityQueue;
+
+/* 문제
+절댓값 힙은 다음과 같은 연산을 지원하는 자료구조이다.
+
+배열에 정수 x (x ≠ 0)를 넣는다.
+배열에서 절댓값이 가장 작은 값을 출력하고, 그 값을 배열에서 제거한다. 절댓값이 가장 작은 값이 여러개일 때는, 가장 작은 수를 출력하고, 그 값을 배열에서 제거한다.
+프로그램은 처음에 비어있는 배열에서 시작하게 된다.
+ */
+/* 입력
+첫째 줄에 연산의 개수 N(1≤N≤100,000)이 주어진다.
+다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다.
+만약 x가 0이 아니라면 배열에 x라는 값을 넣는(추가하는) 연산이고,
+x가 0이라면 배열에서 절댓값이 가장 작은 값을 출력하고 그 값을 배열에서 제거하는 경우이다.
+입력되는 정수는 -231보다 크고, 231보다 작다.
+ */
+// 출력
+// 입력에서 0이 주어진 회수만큼 답을 출력한다. 만약 배열이 비어 있는 경우인데 절댓값이 가장 작은 값을 출력하라고 한 경우에는 0을 출력하면 된다.
+
+/*
+예시 입력
+18
+1
+-1
+0
+0
+0
+1
+1
+-1
+-1
+2
+-2
+0
+0
+0
+0
+0
+0
+0
+ */
+
+/*
+예시 출력
+-1
+1
+0
+-1
+-1
+1
+1
+-2
+2
+0
+ */
+
+
 
 class Solution {
     public void solve() throws IOException {
-        System.setIn(new FileInputStream("input.txt경로"));
+
+        System.setIn(new FileInputStream("C:\\01.lab\\01.java\\coding_test\\src\\backjoon\\Java\\silver\\solutions\\absolutevalueheap\\input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int absA = Math.abs(a);
+            int absB = Math.abs(b);
+
+            if (absA > absB){
+                return b;
+            } else if (absA<absB) {
+                return a;
+            } else {
+                return a>b?b:a;
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0){
+                pq.add(num);
+            }else{
+                if (pq.isEmpty()){
+                    bw.write("0\n");
+                }else {
+                    bw.write(pq.poll() + "\n");
+                }
+            }
+        }
+        bw.flush();
 
     }
-
 }
-
-
 public class Main {
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
